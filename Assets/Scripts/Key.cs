ae5599aec0a4d39e5407fd9b65c7c03e89d7d2de
daemon29 <34591979+daemon29@users.noreply.github.com>
2019-08-31 19:51:08 +0700

    Master

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,39 +5,46 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    public float minSwipeDistY;
-
-    public float minSwipeDistX, speed = 10;
-
-    private Vector2 startPos;
-
-    public GameObject character;
+    CharacterController cc;
+    public Camera cam;
+    public float movespeed = 5.5f;
+    float gravity = 0f;
     // Use this for initialization
     void Start()
     {
-
-
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        Movement();
+    }
+    void Movement()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(x, 0, z).normalized;
+        float cameraDirection = cam.transform.localEulerAngles.y;
+        direction = Quaternion.AngleAxis(cameraDirection, Vector3.up) * direction;
+        Vector3 velecity = direction * movespeed * Time.deltaTime;
+
+        if (cc.isGrounded)
         {
-            character.transform.position += new Vector3(-Time.deltaTime * 6f, 0, 0);
+            gravity = 0;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        else
         {
-            character.transform.position += new Vector3(Time.deltaTime * 6f, 0, 0);
+            gravity += 0.25f;
+            gravity = Mathf.Clamp(gravity, 1f, 20f);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        Vector3 gravityVector = -Vector3.up * gravity * Time.deltaTime;
+        cc.Move(velecity + gravityVector);
+        if (velecity.magnitude > 0)
         {
-            character.transform.position += new Vector3(0, 0, Time.deltaTime * 6f);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            character.transform.position += new Vector3(0, 0, -Time.deltaTime * 6f);
+            //float yAngel = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            //transform.localEulerAngles = new Vector3(0, yAngel, 0);
         }
     }
-
 }
